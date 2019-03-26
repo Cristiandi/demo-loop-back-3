0ad3027fb114d0af65492c254c9bae715408fa66
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2019-03-26 18:05:54 -0500

    feat(demo): sending remote method

--- common/models/my-user.js
+++ common/models/my-user.js
@@ -2,4 +2,36 @@ const timestamp = require('../../server/mixins/timestamp');
 
 module.exports = MyUser => {
   timestamp(MyUser, {});
+
+  MyUser.greet = req => {
+    const { name } = req.query;
+    if (!name) {
+      const err = new Error('name is required in query paramameters');
+      err.status = 422;
+      throw err;
+    }
+
+    return Promise.resolve(`Greetings... ${name}!`);
+  };
+  MyUser.remoteMethod('greet', {
+    accepts: {
+      arg: 'req',
+      type: 'object',
+      http: {
+        source: 'req'
+      }
+    },
+    returns: {
+      arg: 'greeting',
+      type: 'string',
+      root: true
+    },
+    http: {
+      path: '/greet',
+      verb: 'get',
+      status: 200,
+      errorStatus: 500
+    },
+    description: 'Greet someone.'
+  });
 };
